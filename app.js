3a8e39ab71c410e41974751cf3d970b531183fc3
sajan1231 <ashutoshsajan1213@gmail.com>
2020-07-03 16:50:36 +0530

    minor changes

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 const formatMsg = require("./utils/message");
+const chalk = require("chalk");
 const Chat = require("./models/Chat");
 
 const db = require("./config/connectDb");
@@ -18,20 +19,21 @@ const {
 	userLeave,
 	getRoomUsers,
 } = require("./utils/users");
-const { model } = require("./models/Chat");
 
-const bot = "chatBot";
+const BOT = "CHAT_BOT";
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", (socket) => {
 	socket.on("joinRoom", ({ username, room }) => {
+		// console.log(chalk.red.bold("check 1 on connection"));
+
 		const user = userJoin(socket.id, username, room);
 		socket.join(user.room);
 
 		// emiting to a chat room
 		socket.emit(
 			"message",
-			formatMsg(bot, `hello ${user.username} welcome to chat`)
+			formatMsg(BOT, `hello ${user.username} welcome to chat`)
 		);
 
 		// bradcast when an user connects
@@ -39,7 +41,7 @@ io.on("connection", (socket) => {
 		// brodacasting to a specific chatroom
 		socket.broadcast
 			.to(user.room)
-			.emit("message", formatMsg(bot, `${user.username} has joind the chat`));
+			.emit("message", formatMsg(BOT, `${user.username} has joind the chat`));
 
 		// send users and room info to client
 		io.to(user.room).emit("roomUsers", {
@@ -51,14 +53,14 @@ io.on("connection", (socket) => {
 	// console.log(socket, "socket connected");
 	// welcome new user
 	// broadcast to single user
-	// socket.emit("message", formatMsg(bot, "hello welcome to chat")); // moved inside joinRoom to badcast to a specific chat room
+	// socket.emit("message", formatMsg(BOT, "hello welcome to chat")); // moved inside joinRoom to badcast to a specific chat room
 
 	// bradcast when an user connects
 	// emit to all the clients accept user
 	// broadcast to everywhere
 	// socket.broadcast.emit(
 	// 	"message",
-	// 	formatMsg(bot, "an user has joind the chat")
+	// 	formatMsg(BOT, "an user has joind the chat")
 	// );// moved inside joinRoom to badcast to a specific chat room
 
 	// listen for chat msg send from the client
@@ -70,9 +72,11 @@ io.on("connection", (socket) => {
 
 	// v2 after implimenting rooms functionality
 	socket.on("chatMsg", async (msg) => {
+		// console.log(chalk.red.bold("check 2 on msg"));
+
 		const user = getCurrnetUser(socket.id);
 		const chat = await Chat.create({ username: user.username, msg });
-		await chat.save();
+		chat.save();
 		io.to(user.room).emit("message", formatMsg(user.username, msg));
 	});
 
@@ -80,17 +84,19 @@ io.on("connection", (socket) => {
 	// v1 before implimenting rooms functionality
 	// socket.on("disconnect", () => {
 	// 	// emit to all the clients
-	// 	io.emit("message", formatMsg(bot, "an user has left the chat"));
+	// 	io.emit("message", formatMsg(BOT, "an user has left the chat"));
 	// });
 
 	// v2 after implimenting rooms functionality
 	socket.on("disconnect", () => {
+		// console.log(chalk.red.bold("check 3 on disconnect"));
+
 		const user = userLeave(socket.id);
 		if (user) {
 			// emit to all the clients
 			io.to(user.room).emit(
 				"message",
-				formatMsg(bot, `${user.username} has left the chat`)
+				formatMsg(BOT, `${user.username} has left the chat`)
 			);
 
 			// send users and rooms info
